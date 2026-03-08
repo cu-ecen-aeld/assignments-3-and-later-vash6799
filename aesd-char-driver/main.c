ef0b1d2df523f884596da2db6d7e124221073c99
Varun Shah <vash6799@colorado.edu>
2026-03-08 17:18:33 -0600

    Added my name in main.c

--- aesd-char-driver/main.c
+++ aesd-char-driver/main.c
@@ -27,7 +27,7 @@
 int aesd_major =   0; // use dynamic major
 int aesd_minor =   0;
 
-MODULE_AUTHOR("Your Name Here"); /** TODO: fill in your name **/
+MODULE_AUTHOR("Varun Shah"); /** TODO: fill in your name **/
 MODULE_LICENSE("Dual BSD/GPL");
 
 struct aesd_dev aesd_device;
